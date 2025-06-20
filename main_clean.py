a788894cd6e48ab8faf78559cbb31777db2f000b
sheng-luen-chung <sheng.luen.chung@gmail.com>
2025-06-20 16:28:16 +0800

    Refactor GitHub Actions workflow and remove deprecated scripts

---
+++ main_clean.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Regular Comix - Clean Main Script
+簡化版主程式，專為 GitHub Actions 設計
+"""
+
+# 直接導入並執行主程式
+from main import main
+
+if __name__ == "__main__":
+    main()
